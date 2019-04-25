4b5dc05fc06984f125be76b883251c9f89290c61
Nicolas <37118153+Nickeroro@users.noreply.github.com>
2019-04-25 23:33:13 +0200

    Update views.py

--- NASAGRAM/home/views.py
+++ NASAGRAM/home/views.py
@@ -18,7 +18,7 @@ def home(request):
     return render(request, 'home.html', {'background': parsedData})
 
 
-def picsviewer(request, earth_date, camera_name):
+def picsviewer_old(request, earth_date, camera_name):
     parsedData = []
     print("ok")
     if request.method == 'GET':
@@ -35,3 +35,40 @@ def picsviewer(request, earth_date, camera_name):
 
             parsedData.append(nasaData)
         return render(request, 'home.html', {'pics': parsedData})
+    
+def picsviewer(request, earth_date, camera_name):
+    parsedData = []
+
+    if request.method == 'GET':
+        api_key = "&api_key=pEqx0KfvYk6o3MbDGmFMxgMvb4rFhndc2eXyZoqx"
+        req = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=" + earth_date + api_key
+        response = requests.get(req)
+        todo = json.loads(response.text)
+        nasaData = {}
+
+        for pics in todo['photos']:
+            EARTH_DATE = pics['earth_date']
+            nasaData['earth_date'] = pics['earth_date']
+
+            CAMERA_NAME = pics['camera']['name']
+            nasaData['camera_name'] = pics['camera']['name']
+
+            CAMERA_ID = pics['id']
+            nasaData['id'] = pics['id']
+
+            IMG_SRC = pics['img_src']
+            nasaData['img_src'] = pics['img_src']
+            buffer = CAMERA_ID
+            parsedData.append(nasaData)
+
+            if NasaData.objects.filter(CAMERA_ID=buffer).exists():
+                print("Object already exists in DataBase, don't save anything")
+            else:
+                nasa_data = NasaData(CAMERA_ID=CAMERA_ID,
+                                     CAMERA_NAME=CAMERA_NAME,
+                                     EARTH_DATE=EARTH_DATE,
+                                     IMG_SRC=IMG_SRC)
+                nasa_data.save()
+
+
+        return render(request, 'home.html', {'pics': parsedData})    
