083012eda5f69309db06caa96e0b3f85be562760
Tony CALVEZ <tony.calvez@hotmail.fr>
2019-04-25 22:47:56 +0200

    Add files via upload

--- NASAGRAM/home/models.py
+++ NASAGRAM/home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class NasaData(models.Model):
+    CAMERA_ID = models.IntegerField()
+    CAMERA_NAME = models.CharField(max_length=100)
+    EARTH_DATE = models.CharField(max_length=20)
+    IMG_SRC = models.CharField(max_length=100)
+
+    objects = models.Manager()
