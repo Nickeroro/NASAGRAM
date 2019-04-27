b7d4034ef4035b8fde6eed8caac9c919403b8a80
Nicolas <37118153+Nickeroro@users.noreply.github.com>
2019-04-27 23:18:53 +0200

    Update start.sh

--- NASAGRAM/start.sh
+++ NASAGRAM/start.sh
@@ -5,18 +5,18 @@ while getopts "mrh" option; do
 	case $option in
 		m)
 		python manage.py makemigrations home
-    python manage.py migrate
-    python manage.py runserver
+   		python manage.py migrate
+   		python manage.py runserver
 		;;
 		r)
 		python manage.py runserver
 		;;
-    h)
+    		h)
 		echo "Command help - start nasagram "
 		echo "========================="
 		echo "-m command: make migrations and run the server"
 		echo "-r command: only run the server" 
-    echo "-h command: prompt help"
+  		echo "-h command: prompt help"
 		;;
 		?)
 		echo "illegal option :("
